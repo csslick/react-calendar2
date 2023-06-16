af50fe9f9be148bad010546cf6a079e511b6fded
csslick <digitone@naver.com>
2023-06-16 17:05:41 +0900

    이전달 다음달  & 해넘김 기능

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,36 @@ function App() {
   const [date, setDate] = useState(new Date().getDate());
   console.log(year, month, date);
 
+  // 다음달 
+  const nextMonth = () => {
+    if(month === 11) {
+      setMonth(0); // 1월
+      setYear(year + 1); // 다음 년도  
+      return;
+    }
+    setMonth(month + 1);
+  }
+
+  // 이전달
+  const prevMonth = () => {
+    if(month === 0) {
+      setMonth(11);
+      setYear(year - 1); // 이전년도
+      return;
+    }
+    setMonth(month - 1);
+  }
+
+
   return (
     <div className='App'>
       <Header/>
       {/* 년-월-일 state를 카렌더 컨포너트에 전달 */}
       <Calendar year={year} month={month} date={date}/>
+      <div className="btn-group">
+        <button className="prev" onClick={prevMonth}>이전 달</button>
+        <button className="next" onClick={nextMonth}>다음 달</button>
+      </div>
     </div>
   )
 }
